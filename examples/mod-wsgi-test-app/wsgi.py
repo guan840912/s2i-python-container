88f29680ea759f94d3d329a50cdbc453fe799db8
Neil Guan <46012524+guan840912@users.noreply.github.com>
2019-10-14 13:36:58 +0800

    Update wsgi.py

--- examples/mod-wsgi-test-app/wsgi.py
+++ examples/mod-wsgi-test-app/wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return b'Hello World from mod_wsgi hosted WSGI application!'
+    return b'Hello World from mod_wsgi hosted WSGI application! test insatll other package ~~  '
 
 if __name__ == '__main__':
     application.run()
