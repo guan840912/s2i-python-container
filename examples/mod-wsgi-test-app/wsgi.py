05b9278e52235d41db5477e68b1e420859e84596
Neil Guan <46012524+guan840912@users.noreply.github.com>
2019-10-18 13:23:03 +0800

    Update wsgi.py

--- examples/mod-wsgi-test-app/wsgi.py
+++ examples/mod-wsgi-test-app/wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return b'Hello World from mod_wsgi hosted WSGI application! test insatll other package ~~  '
+    return b'Hello World from mod_wsgi hosted WSGI application! test insatll other package ~~ add webhook  '
 
 if __name__ == '__main__':
     application.run()
